cee35b35fe6fbf2a0770ddec43daf6721e76b4e8
imfaisii <i160289@nu.edu.pk>
2022-01-22 01:08:16 +0500

    Update AdminSeeder.php

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -18,18 +18,18 @@ class AdminSeeder extends Seeder
      */
     public function run()
     {
-        $user = User::create([
-            'name' => 'Admin',
-            'email' => 'i160289@nu.edu.pk',
-            'password' => Hash::make('12345678'),
-            'role' => 'admin'
-        ]);
-        $user->addMedia(public_path('assets/img/avatar.png'))->preservingOriginal()->toMediaCollection('avatars', 'avatars');
+        // $user = User::create([
+        //     'name' => 'Admin',
+        //     'email' => 'i160289@nu.edu.pk',
+        //     'password' => Hash::make('12345678'),
+        //     'role' => 'admin'
+        // ]);
+        // $user->addMedia(public_path('assets/img/avatar.png'))->preservingOriginal()->toMediaCollection('avatars', 'avatars');
 
         $user = User::create([
             'name' => 'Admin',
             'email' => 'hr@grapegames.net',
-            'password' => Hash::make('12345678'),
+            'password' => Hash::make('google@me'),
             'role' => 'admin'
         ]);
         $user->addMedia(public_path('assets/img/avatar.png'))->preservingOriginal()->toMediaCollection('avatars', 'avatars');
