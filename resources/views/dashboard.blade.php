0714ba25751064f48d5dda6b958771ea2fa9b4ec
imfaisii <i160289@nu.edu.pk>
2022-02-05 13:19:54 +0500

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -42,7 +42,7 @@
         $(function() {
             var intervalId = window.setInterval(function() {
                 $('#myChart-license-text').fadeOut('fast').css("display", "none !important;");
-            }, 100); // <-- time in milliseconds
+            }, 1);
         });
     </script>
     @if (session()->has('toast'))
