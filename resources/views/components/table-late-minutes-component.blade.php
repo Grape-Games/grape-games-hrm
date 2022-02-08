2ced4a2c791ab171c1901ffde2b54153c47068b3
imfaisii <i160289@nu.edu.pk>
2022-02-08 13:42:32 +0500

    late minutes

---
+++ resources/views/components/table-late-minutes-component.blade.php
@@ -0,0 +1,18 @@
+<div class="row">
+    <div class="col-sm-12">
+        <div class="card mb-0">
+            <div class="card-header">
+                <h4 class="card-title mb-0">Late Minutes Table</h4>
+                <p class="card-text">
+                    This table show a list of all
+                    <code>late minutes</code> in the system.
+                </p>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-stripped mb-0 lm-table"></table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
