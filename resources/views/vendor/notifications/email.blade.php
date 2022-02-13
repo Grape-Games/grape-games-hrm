1699e1c6bbc8207ca4edcf769644d381b5926da1
imfaisii <i160289@nu.edu.pk>
2022-02-14 00:13:58 +0500

    Update email.blade.php

--- resources/views/vendor/notifications/email.blade.php
+++ resources/views/vendor/notifications/email.blade.php
@@ -216,7 +216,7 @@
             [
             'actionText' => $actionText,
             ]
-            )
+            ) <span class="break-all">[{{ $displayableActionUrl }}]</span>
         @endisset
     </div>
 </body>
