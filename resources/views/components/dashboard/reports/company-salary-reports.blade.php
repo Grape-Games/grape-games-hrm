682fe89818a322b1fc959ab52a486aafeb1f3db8
Faisal Ashfaq <59202459+imfaisii@users.noreply.github.com>
2022-06-01 19:30:25 +0000

    other-

--- resources/views/components/dashboard/reports/company-salary-reports.blade.php
+++ resources/views/components/dashboard/reports/company-salary-reports.blade.php
@@ -350,7 +350,7 @@
 
     $(function() {
         reCal();
-        makeDTnAjax("main-table", "A0");
+        makeDTnAjaxCols("main-table", "A1", [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,20,21,22,23,24,25,26,27,28,29,30]);
 
 
         var date = "{{ request()->get('date') }}";
